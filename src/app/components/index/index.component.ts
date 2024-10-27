3b2c592f1198dcb7a609a6a6c5db1361eaa152e6
mona <monamt1368@gmail.com>
2024-10-27 11:05:16 +0330

    NJT-6501 monaNJT-10826 : recent-events.component new UI

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
+import {RecentEventsComponent} from "./recent-events/recent-events.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    RecentEventsComponent
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
