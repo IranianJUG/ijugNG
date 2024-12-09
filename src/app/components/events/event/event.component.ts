feacc9da8242831951624836253ddb654de81a61
mohammad.m <mmohammadmatloub@gmail.com>
2024-12-09 16:14:03 +0330

    feat: improve-event-page-ui

--- src/app/components/events/event/event.component.ts
+++ src/app/components/events/event/event.component.ts
@@ -1,30 +1,79 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FooterComponent} from "../../index/footer/footer.component";
-import {HeaderComponent} from "../../index/header/header.component";
-import {EventService} from "../../../services/event.service";
-import {EventInterface} from "../../../interfaces/event";
+import { Component, Input, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+import { Carousel } from 'primeng/carousel';
+import * as L from 'leaflet';
+
+import { FooterComponent } from '../../index/footer/footer.component';
+import { HeaderComponent } from '../../index/header/header.component';
+import { EventService } from '../../../services/event.service';
+import { EventInterface } from '../../../interfaces/event';
 
 @Component({
   selector: 'app-event',
-  standalone: true,
-  imports: [
-    FooterComponent,
-    HeaderComponent
-  ],
   templateUrl: './event.component.html',
-  styleUrl: './event.component.scss'
+  styleUrl: './event.component.scss',
+  standalone: true,
+  imports: [FooterComponent, HeaderComponent, Carousel, DatePipe],
 })
 export class EventComponent implements OnInit {
-    @Input() slug!: string
-    event : EventInterface | undefined
+  @Input() slug!: string;
+  event: EventInterface | undefined;
+  eventLoc = { lat: 35.6895, lng: 51.389 };
+  sanitizedDescription!: SafeHtml;
 
-    constructor(private eventService: EventService) {
-    }
+  galleryContent: any[] = [
+    {
+      content: `<div class="items-center justify-center gap-4">
+                    <div class=" flex w-full items-center justify-center">
+                        <img src="./imgs/img-1.jpg" alt="developer">
+                    </div>
+               </div>`,
+    },
+    {
+      content: `<div class="items-center justify-center gap-4">
+                    <div class=" flex w-full items-center justify-center">
+                        <img src="./imgs/img-2.jpg"+""+ "alt="developer">
+                    </div>
+               </div>`,
+    },
+  ];
 
-    ngOnInit() {
-      console.log(this.slug)
-      this.eventService.getOne(this.slug).subscribe((response)=>{
-        this.event = response.data
-      })
-    }
+  constructor(
+    private eventService: EventService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.eventService.getOne(this.slug).subscribe((response) => {
+      this.event = response.data;
+      if (this.event?.description) {
+        this.sanitizedDescription = this.sanitizer.bypassSecurityTrustHtml(
+          this.decodeHtml(this.event?.description)
+        );
+      }
+    });
+
+    const map = L.map('map').setView(
+      [this.eventLoc.lat, this.eventLoc.lng],
+      13
+    );
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([this.eventLoc.lat, this.eventLoc.lng])
+      .addTo(map)
+      .bindPopup('مکان رویداد')
+      .openPopup();
+  }
+
+  private decodeHtml(encodedStr: string): string {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = encodedStr;
+    return textArea.value;
+  }
 }
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {animate, style, transition, trigger} from "@angular/animations";
-import {NgOptimizedImage} from "@angular/common";
-import {Carousel} from "primeng/carousel";
-import {TranslateModule} from "@ngx-translate/core";
-import {FooterComponent} from "./footer/footer.component";
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+import { Carousel } from 'primeng/carousel';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-index',
@@ -21,129 +23,144 @@ import {FooterComponent} from "./footer/footer.component";
   animations: [
     trigger('slideInLeft', [
       transition(':enter', [
-        style({transform: 'translateX(-100%)'}),
-        animate('200ms ease-in', style({transform: 'translateX(0)'})),
+        style({ transform: 'translateX(-100%)' }),
+        animate('200ms ease-in', style({ transform: 'translateX(0)' })),
       ]),
-      transition(":leave", [
-        animate('200ms ease-out', style({transform: 'translateX(-100%)'})),
-      ])
-    ])
+      transition(':leave', [
+        animate('200ms ease-out', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
   ],
 })
 export class IndexComponent {
-
   sliderContent: any = [
     {
       content: `<div class="items-center justify-center gap-4">
                     <div class=" flex w-full items-center justify-center">
                         <img src="./imgs/img-1.jpg" alt="developer">
                     </div>
-               </div>`
+               </div>`,
     },
     {
       content: `<div class="items-center justify-center gap-4">
                     <div class=" flex w-full items-center justify-center">
                         <img src="./imgs/img-2.jpg" alt="developer">
                     </div>
-               </div>`
-    }
-  ]
-  sliderIndex: number = 0
-  recentEvents = ['هفدهمین رویداد تهران جاگ','شانزدهمین رویداد تهران جاگ','جشن روز برنامه نویس','جلسه پانزدهم تهران جاگ','جلسه چهاردهم تهران جاگ']
-  popularCategory= [
-    'Javascript','TypeScript','JAVA','OracleDB','Java 23',';)'
-  ]
+               </div>`,
+    },
+  ];
+  sliderIndex: number = 0;
+  recentEvents = [
+    'هفدهمین رویداد تهران جاگ',
+    'شانزدهمین رویداد تهران جاگ',
+    'جشن روز برنامه نویس',
+    'جلسه پانزدهم تهران جاگ',
+    'جلسه چهاردهم تهران جاگ',
+  ];
+  popularCategory = [
+    'Javascript',
+    'TypeScript',
+    'JAVA',
+    'OracleDB',
+    'Java 23',
+    ';)',
+  ];
 
-  recentInstructor=[
+  recentInstructor = [
     {
-      name:"مجید مصطفوی",
-      title:"Experienced Java Developer and JUG Leader with a strong" +
-        " background in building efficient, high-performance software solutions. Committed to community engagement and professional development." ,
-      image:"https://ijug.ir/team/Majid-1.jpg",
+      name: 'مجید مصطفوی',
+      title:
+        'Experienced Java Developer and JUG Leader with a strong' +
+        ' background in building efficient, high-performance software solutions. Committed to community engagement and professional development.',
+      image: 'https://ijug.ir/team/Majid-1.jpg',
     },
     {
-      name:"حسام غیاصی",
-      title:"Software Engineer | Specializing in Java, Spring, Hibernate," +
-        " software architecture, TDD, DDD and agile | Leading development teams to higher quality and better productivity | Lifelong researcher and learner",
-      image:"https://ijug.ir/team/Hesam.jpg",
+      name: 'حسام غیاصی',
+      title:
+        'Software Engineer | Specializing in Java, Spring, Hibernate,' +
+        ' software architecture, TDD, DDD and agile | Leading development teams to higher quality and better productivity | Lifelong researcher and learner',
+      image: 'https://ijug.ir/team/Hesam.jpg',
     },
     {
-      name:"فرناز مثیمی",
-      title:"Java Developer",
-      image:"https://ijug.ir/team/Farnaz.png",
+      name: 'فرناز مثیمی',
+      title: 'Java Developer',
+      image: 'https://ijug.ir/team/Farnaz.png',
     },
     {
-      name:"امین بازگیر",
-      title:"Java Developer",
-      image:"https://ijug.ir/team/AminB.jpg",
+      name: 'امین بازگیر',
+      title: 'Java Developer',
+      image: 'https://ijug.ir/team/AminB.jpg',
     },
     {
-      name:"امیررضا رضائیان",
-      title:"Java Developer",
-      image:"https://ijug.ir/team/AmirReza.jpg",
-    }
-  ]
+      name: 'امیررضا رضائیان',
+      title: 'Java Developer',
+      image: 'https://ijug.ir/team/AmirReza.jpg',
+    },
+  ];
 
-  ourSponsors=[
+  ourSponsors = [
     {
-      name:"راهبرد هوشمند شهر",
-      image:"./sponsors/smartcity.png",
-      bg:"#fff"
-    },{
-      name:"داده پردازان ارتباط گستر ویونا",
-      image:"./sponsors/viona.png",
-      bg:"#fff"
-    },{
-      name:"ایران نیک",
-      image:"./sponsors/irnic.png",
-      bg:"#4745EE"
-    },{
-      name:"پژوهشگاه دانش های بنیادی",
-      image:"./sponsors/ipm.png",
-      bg:"#fff"
-    },{
-      name:"ایران نت",
-      image:"./sponsors/iran-net.png",
-      bg:"#4745EE"
-    }
+      name: 'راهبرد هوشمند شهر',
+      image: './sponsors/smartcity.png',
+      bg: '#fff',
+    },
+    {
+      name: 'داده پردازان ارتباط گستر ویونا',
+      image: './sponsors/viona.png',
+      bg: '#fff',
+    },
+    {
+      name: 'ایران نیک',
+      image: './sponsors/irnic.png',
+      bg: '#4745EE',
+    },
+    {
+      name: 'پژوهشگاه دانش های بنیادی',
+      image: './sponsors/ipm.png',
+      bg: '#fff',
+    },
+    {
+      name: 'ایران نت',
+      image: './sponsors/iran-net.png',
+      bg: '#4745EE',
+    },
   ];
 
   responsiveOptions = [
     {
       breakpoint: '1400px',
       numVisible: 2,
-      numScroll: 1
+      numScroll: 1,
     },
     {
       breakpoint: '1199px',
       numVisible: 3,
-      numScroll: 1
+      numScroll: 1,
     },
     {
       breakpoint: '767px',
       numVisible: 2,
-      numScroll: 1
+      numScroll: 1,
     },
     {
       breakpoint: '575px',
       numVisible: 1,
-      numScroll: 1
-    }
-  ]
+      numScroll: 1,
+    },
+  ];
 
-  prevSlide() {
-    console.log('prev', this.sliderIndex)
+  prevSlide(): void {
+    console.log('prev', this.sliderIndex);
 
     if (this.sliderIndex > 0) {
-      this.sliderIndex -= 1
+      this.sliderIndex -= 1;
     }
   }
 
-  nextSlide() {
-    console.log('next', this.sliderIndex)
+  nextSlide(): void {
+    console.log('next', this.sliderIndex);
     if (this.sliderIndex < this.sliderContent.length - 1) {
-      this.sliderIndex += 1
+      this.sliderIndex += 1;
     }
   }
-
 }
