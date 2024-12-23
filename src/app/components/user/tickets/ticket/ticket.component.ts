446425c541bdb299a286eabc8403b90f27646fea
mohammad.m <mmohammadmatloub@gmail.com>
2024-12-23 10:47:35 +0330

    feat: improve-ticket-detail-page-ui

--- src/app/components/user/tickets/ticket/ticket.component.ts
+++ src/app/components/user/tickets/ticket/ticket.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { jsPDF } from 'jspdf';
+
 import { TicketService } from '../../../../services/ticket.service';
 import { Ticket, TicketResponse } from '../../../../interfaces/ticket';
+import { PersianDatePipe } from '../../../../pipes/persian-date.pipe';
 
 @Component({
   selector: 'app-ticket',
-  standalone: true,
-  imports: [],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.scss',
+  standalone: true,
+  imports: [PersianDatePipe],
 })
 export class TicketComponent implements OnInit {
   ticket: Ticket | null = null;
@@ -24,7 +27,29 @@ export class TicketComponent implements OnInit {
     this.getTicketDetails(ticketId);
   }
 
-  getTicketDetails(id: number): void {
+  // protected downloadPDF() {
+  //   const doc = new jsPDF({
+  //     unit: 'mm',
+  //     format: 'a5',
+  //     orientation: 'portrait',
+  //   });
+
+  //   const content = document.getElementById('ticket-content');
+
+  //   if (content) {
+  //     doc.html(content, {
+  //       callback: function (doc) {
+  //         doc.save('ticket.pdf');
+  //         doc.
+  //       },
+  //       margin: [10, 10, 10, 10],
+  //       x: 10,
+  //       y: 10,
+  //     });
+  //   }
+  // }
+
+  private getTicketDetails(id: number): void {
     this.ticketService.getTicket(id).subscribe((ticket: TicketResponse) => {
       this.ticket = ticket.data;
     });
--- src/app/interfaces/event.ts
+++ src/app/interfaces/event.ts
@@ -2,31 +2,31 @@ export interface EventInterface {
   id: number;
   name: string;
   slug: string;
-  thumbnail?:string;
-  image:string;
-  gallery?:string[];
-  description?:string;
-  tags?:string[];
-  total_count?:number;
-  remain_count?:number;
-  short_description?:string;
-  event_time:string;
-  location_name?:string;
-  location_address?:string;
-  location_lat?:string;
-  location_lng?:string;
-  price?:number;
-  created_at?:number;
-  updated_at?:number;
-  hasTicket?:boolean;
+  thumbnail?: string;
+  image: string;
+  gallery?: string[];
+  description?: string;
+  tags?: string[];
+  total_count?: number;
+  remain_count?: number;
+  short_description?: string;
+  event_time: string;
+  location_name?: string;
+  location_address?: string;
+  location_lat?: string;
+  location_lng?: string;
+  price?: number;
+  created_at?: string;
+  updated_at?: number;
+  hasTicket?: boolean;
 }
 
 export interface EventResponseInterface {
-  success:boolean;
-  data?:EventInterface;
+  success: boolean;
+  data?: EventInterface;
 }
 
 export interface EventsResponseInterface {
-  success:boolean;
-  data:EventInterface[];
+  success: boolean;
+  data: EventInterface[];
 }
